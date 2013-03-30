ea67bb9978a814b93184cbb42310585640ec02fd
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2013-03-31 01:59:19 +0200

    Add speclj to deps and plugins

--- project.clj
+++ project.clj
@@ -1,9 +1,11 @@
 (defproject clotodo "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "A todo webapp in clojure"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [compojure "1.1.5"]]
   :plugins [[lein-ring "0.8.2"]]
   :ring {:handler clotodo.handler/app}
   :profiles
-  {:dev {:dependencies [[ring-mock "0.1.3"]]}})
+  {:dev {:dependencies [[ring-mock "0.1.3"], [speclj "2.5.0"]]}}
+  :plugins [[speclj "2.5.0"]]
+  :test-paths ["spec/"])
