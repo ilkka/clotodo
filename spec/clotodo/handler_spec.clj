80652dfccf54f7447b8f3a9a85aa6ef184d62838
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2013-03-31 22:55:30 +0300

    Restructure specs
    It seems a (let) between (describe) and (it) is bad and will cause some specs not to be run.

--- spec/clotodo/handler_spec.clj
+++ spec/clotodo/handler_spec.clj
@@ -1,17 +1,25 @@
 (ns clotodo.handler_spec
   (:use speclj.core
         ring.mock.request
-        clotodo.handler))
+        clotodo.handler
+        cheshire.core))
 
 (describe "clotodo.handler"
   (it "serves up the app UI from the root"
     (let [response (app (request :get "/"))]
       (should= 200 (:status response))))
 
-  (describe "/todo API"
-    (let [response (app (request :get "/todo"))]
+  (let [response (app (request :get "/todo"))]
+    (describe "/todo API"
       (it "has a /todo API call"  
         (should= 200 (:status response)))
-      (it "responds with todo JSON"
-        (should= "[{\"label\":\"Buymilk\",\"createdAt\":\"2013-01-01T12:00:00Z\"},{\"label\":\"Buyeggs\",\"createdAt\":\"2013-01-02T12:00:00Z\"},{\"label\":\"LearnAngular.js\",\"createdAt\":\"2013-01-04T12:00:00Z\"},{\"label\":\"LearnClojure\",\"createdAt\":\"2013-01-03T12:00:00Z\"}]" (:body response)))))
+      (it "responds with valid JSON"
+        (should-not-throw (parse-string (:body response) true)))
+
+      (let [todos (parse-string (:body response) true)]
+        (describe "todo JSON"
+          (it "should not be nil"
+            (should-not= nil todos))
+          (it "should be list"
+            (should= clojure.lang.PersistentVector (type todos)))))))
   )
