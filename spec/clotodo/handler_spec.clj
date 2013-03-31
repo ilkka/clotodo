7a01a052e40ce2e1b08d16f8171dd3f618f7ade4
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2013-03-31 21:50:50 +0300

    Silly check for the todo list api

--- spec/clotodo/handler_spec.clj
+++ spec/clotodo/handler_spec.clj
@@ -8,6 +8,10 @@
     (let [response (app (request :get "/"))]
       (should= 200 (:status response))))
 
-  (it "has a /todo API call"
+  (describe "/todo API"
     (let [response (app (request :get "/todo"))]
-      (should= 200 (:status response)))))
+      (it "has a /todo API call"  
+        (should= 200 (:status response)))
+      (it "responds with todo JSON"
+        (should= "[{\"label\":\"Buymilk\",\"createdAt\":\"2013-01-01T12:00:00Z\"},{\"label\":\"Buyeggs\",\"createdAt\":\"2013-01-02T12:00:00Z\"},{\"label\":\"LearnAngular.js\",\"createdAt\":\"2013-01-04T12:00:00Z\"},{\"label\":\"LearnClojure\",\"createdAt\":\"2013-01-03T12:00:00Z\"}]" (:body response)))))
+  )
