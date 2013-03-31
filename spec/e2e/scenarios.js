f52a8235bf0f1688f778c60838a8cb014964a097
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2013-03-31 18:11:46 +0300

    Test for filtering

--- spec/e2e/scenarios.js
+++ spec/e2e/scenarios.js
@@ -9,4 +9,16 @@ describe('clotodo', function () {
   it('should show a count of 4', function() {
     expect(element('.todo-count').text()).toBe("4");
   });
+
+  it('should show 4 elements in the list', function() {
+    expect(repeater('.todos li').count()).toBe(4);
+  })
+
+  it('should filter the todos when the user enters text into the search box', function() {
+    expect(repeater('.todos li').count()).toBe(4);
+    input('query').enter('learn');
+    expect(repeater('.todos li').count()).toBe(2);
+    input('query').enter('angul');
+    expect(repeater('.todos li').count()).toBe(1);
+  });
 });
