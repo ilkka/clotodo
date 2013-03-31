6db495a816a0529a039a154c0dd97d14c65563fb
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2013-03-31 17:19:17 +0300

    Define our dummy controller with the instance methods

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,22 +1,21 @@
-define(function() {
-  function PhoneListController($scope) {
-    $scope.phones = [
-      {
-        "name"    : "Nexus S",
-        "snippet" : "Blah blah de blah"
-      },
-      {
-        "name"    : "Plexus S",
-        "snippet" : "Blah blah de blah"
-      },
-      {
-        "name"    : "Sexus S",
-        "snippet" : "Blah blah de blah"
-      }
-    ];
-  }
-
-  return {
-    PhoneListController: PhoneListController
-  }
+define(['angular', 'app'], function(angular) {
+  'use strict';
+  angular.module('app').controller('PhoneListController',
+    ['$scope', function($scope) {
+      $scope.phones = [
+        {
+          "name"    : "Nexus S",
+          "snippet" : "Blah blah de blah"
+        },
+        {
+          "name"    : "Plexus S",
+          "snippet" : "Blah blah de blah"
+        },
+        {
+          "name"    : "Sexus S",
+          "snippet" : "Blah blah de blah"
+        }
+      ];
+    }
+  ]);
 });
