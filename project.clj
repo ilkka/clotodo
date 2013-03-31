04f0f755468bb5a0566e11af00aa484ea04e5aae
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2013-03-31 02:05:54 +0200

    Merge duplicate plugins keys

--- project.clj
+++ project.clj
@@ -3,9 +3,8 @@
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [compojure "1.1.5"]]
-  :plugins [[lein-ring "0.8.2"]]
+  :plugins [[lein-ring "0.8.2"], [speclj "2.5.0"]]
   :ring {:handler clotodo.handler/app}
   :profiles
   {:dev {:dependencies [[ring-mock "0.1.3"], [speclj "2.5.0"]]}}
-  :plugins [[speclj "2.5.0"]]
   :test-paths ["spec/"])
