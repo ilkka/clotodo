81df17d43b98e78b071538a0629d9b835b01c4fb
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2013-03-31 18:11:16 +0300

    Shorter sleep is enough for e2e tests to work

--- spec/e2e/scenarios.js
+++ spec/e2e/scenarios.js
@@ -3,7 +3,7 @@ describe('clotodo', function () {
 
   beforeEach(function() {
     browser().navigateTo("/base/public/index.html");
-    sleep(1);
+    sleep(0.2);
   });
 
   it('should show a count of 4', function() {
