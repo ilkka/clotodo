d641020c07303d9a575dd352b7b5eb44f109a9b1
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2013-03-31 14:06:36 +0300

    Test silly controller with silly failing spec

--- spec/unit/dummy_spec.js
+++ spec/unit/dummy_spec.js
@@ -1,12 +1,18 @@
 define([
   'angular',
   'app',
-  ], function(angular, app) {
+  'controllers'
+  ], function(angular, app, controllers) {
     'use strict';
 
-    describe('dummy', function() {
-      it('is silly', function() {
-        expect(true).toBe(true);
+    describe('Controllers', function() {
+      describe('PhoneListController', function() {
+        it('creates a "phones" model with 3 phones', function() {
+          var scope = {},
+            ctrl = new controllers.PhoneListController(scope);
+
+          expect(scope.phones.length).toBe(3);
+        });
       });
     });
   });
