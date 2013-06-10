52355e3f2cfbae6d2ca456e4131f86ca1f845bf8
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2013-06-10 22:47:02 +0300

    Depend on lib-noir

--- project.clj
+++ project.clj
@@ -7,5 +7,5 @@
   :plugins [[lein-ring "0.8.2"], [speclj "2.5.0"]]
   :ring {:handler clotodo.handler/app}
   :profiles
-  {:dev {:dependencies [[ring-mock "0.1.3"], [speclj "2.5.0"]]}}
+  {:dev {:dependencies [[ring-mock "0.1.3"], [speclj "2.5.0"], [lib-noir "0.6.0"]]}}
   :test-paths ["spec/"])
