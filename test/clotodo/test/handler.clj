7dcbe9663ad03fd69ef530a62cbd375f1fb4e6c0
Ilkka Laukkanen <ilkka.laukkanen@futurice.com>
2013-11-01 13:54:33 +0200

    "Fix" the frontpage test

--- test/clotodo/test/handler.clj
+++ test/clotodo/test/handler.clj
@@ -1,14 +1,14 @@
 (ns clotodo.test.handler
   (:use clojure.test
-        ring.mock.request  
+        ring.mock.request
         clotodo.handler))
 
 (deftest test-app
   (testing "main route"
     (let [response (app (request :get "/"))]
       (is (= (:status response) 200))
-      (is (= (:body response) "Hello World"))))
-  
+      (is (not (nil? (re-find #"Welcome to clotodo" (:body response)))))))
+
   (testing "not-found route"
     (let [response (app (request :get "/invalid"))]
       (is (= (:status response) 404)))))
